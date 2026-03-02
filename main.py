a5b413b7811efe23a2da1d16e86ff8d35f76b547
APK-hanal <apilkhanal10@gmail.com>
2026-03-02 21:22:41 +0545

    Fixed import bug with datetime

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
+def main():
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    url = "https://ramropatro.com/vegetable"
+    response = requests.get(url,headers= headers,timeout= 10)
+    response.raise_for_status()
+
+    return response.text
+#web scrape :>
+def parse(response):
+    soup = BeautifulSoup(response, "html.parser")
+    rows = []
+
+    table = soup.find('table', id= 'commodityDailyPrice')
+    if table:
+        row = table.find_all("tr")
+        rows = row
+        # list of dicts
+        data_list = []
+        
+        for element in rows[1:]:
+
+            #indivisual dicts of all the headings
+            data = {}
+            cols = element.find_all("td")
+            if not cols[0].text.strip():
+                continue
+            com = cols[0]
+            unit = cols[1]
+            min_value = cols[2]
+            max_value = cols[3]
+            avg = cols[4]
+            data['Date'] = date.today().isoformat()
+            data["Commodity"] = com.text
+            data['Unit'] = unit.text
+            data['Minimum value'] = min_value.text
+            data['Maximum value'] = max_value.text
+            data['Average'] = avg.text
+            data_list.append(data)
+    return data_list
+
+if __name__ == "__main__":
+    res = main()
+    dicts = parse(res)
+    
