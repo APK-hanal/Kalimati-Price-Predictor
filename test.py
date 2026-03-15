bd828744b7ed4d02d2b7f8706b7f71f3b8279cf4
APK-hanal <apilkhanal10@gmail.com>
2026-03-15 21:39:28 +0545

    Added Train-Test-Split and the random forest Regressor

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from datetime import date,datetime
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 try:
@@ -22,7 +23,13 @@ try:
     df.drop('Date', axis=1, inplace=True)
     le = LabelEncoder()
     df['Commodity'] = le.fit_transform(df['Commodity'])
-    print(df['Commodity'])
+    x = df[['Commodity', 'Year', 'Month', 'Day', 'Minimum value']]
+    y = df['Maximum value']
+    x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.33, random_state=42)
+    model = RandomForestRegressor()
+    model.fit(x_train,y_train)
+    
 except FileNotFoundError:
     print("Error")
 
